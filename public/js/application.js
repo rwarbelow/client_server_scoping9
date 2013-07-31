7ef089a1eb0d294437a7f5ec16f6289afd1b00a7
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-07-30 22:02:23 -0500

    finished

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-  $('.awesomeness_teller').on('click', function() {
 
-  });
+  function GetAwesome(){
+  	$('#awesomeness_holder').empty()
+  	var data = $(this).data('heading')
+	$('#awesomeness_holder').append(data)
+  }
 
+  function GetSkill(){
+  	$('#skill_holder').empty()
+  	var selectedSkill = window.location.pathname.split('/')[2]
+  	$('#skill_holder').append(selectedSkill)
+  }
+
+  function GetMeal(){
+  	$('#meal_holder').empty()
+  	var selectedFood = meals[Math.floor(Math.random()*meals.length)]
+  	$('#meal_holder').append(selectedFood)
+  }
+
+  function EventHandler(){
+  	$('.meal_teller').on('click', GetMeal)
+  	$('.skill_teller').on('click', GetSkill)
+  	$('.awesomeness_teller').on('click', GetAwesome) 
+  }
+  
   // What other events do you need to bind to make the other pages work?
+
+  EventHandler();
 });
+
